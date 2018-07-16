75f6dd469dc3edffe512e0ac377700156df85c7e
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-07-16 22:16:27 +0200

    Created recycler view for steps and loaded them into it

--- app/src/main/java/com/example/android/bakingbuddy/RecipeDetails.java
+++ app/src/main/java/com/example/android/bakingbuddy/RecipeDetails.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.example.android.bakingbuddy.adapters.IngredientFlexibleItem;
+import com.example.android.bakingbuddy.adapters.StepFlexibleItem;
+import com.example.android.bakingbuddy.model.CookingStep;
 import com.example.android.bakingbuddy.model.Ingredient;
 import com.example.android.bakingbuddy.model.Recipe;
 
@@ -24,29 +26,47 @@ public class RecipeDetails extends AppCompatActivity {
 
     private Recipe mInRecipe;
     private RecyclerView mIngredientsRecyclerView;
-    private FlexibleAdapter<IFlexible> mAdapter;
+    private FlexibleAdapter<IFlexible> mIngredientsAdapter;
     private List<IFlexible> mIngredients;
+    private RecyclerView mStepsRecyclerView;
+    private FlexibleAdapter<IFlexible> mStepsAdapter;
+    private List<IFlexible> mSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
 
+//            Initialize Ingredients adapters
         mIngredientsRecyclerView = findViewById(R.id.rv_ingredients);
-        mAdapter = new FlexibleAdapter<>(mIngredients);
-        mIngredientsRecyclerView.setAdapter(mAdapter);
+        mIngredientsAdapter = new FlexibleAdapter<>(mIngredients);
+        mIngredientsRecyclerView.setAdapter(mIngredientsAdapter);
         mIngredientsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false));
         mIngredientsRecyclerView.setHasFixedSize(true);
         mIngredients = new ArrayList<>();
 
+//        Initialize Steps Adapter
+        mStepsRecyclerView = findViewById(R.id.rv_steps);
+        mStepsAdapter = new FlexibleAdapter<>(mSteps);
+        mStepsRecyclerView.setAdapter(mStepsAdapter);
+        mStepsRecyclerView.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mStepsRecyclerView.setHasFixedSize(true);
+        mSteps = new ArrayList<>();
+
         Intent inIntent = getIntent();
         if(inIntent != null){
             mInRecipe = inIntent.getParcelableExtra(KEY_INTENT_CLICKED_RECIPE);
+//            Update Ingredients
             for(Ingredient ingredient : mInRecipe.getIngredients()) {
                 mIngredients.add(new IngredientFlexibleItem(ingredient));
             }
-            mAdapter.updateDataSet(mIngredients);
-//           TODO: Check why the ingredient quantity is always 0
+            mIngredientsAdapter.updateDataSet(mIngredients);
+
+//            Update Steps
+            for(CookingStep step : mInRecipe.getSteps()) {
+                mSteps.add(new StepFlexibleItem(step));
+            }
+            mStepsAdapter.updateDataSet(mSteps);
         }
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
