4138db726f1d59037d7ba9c3150ce73f8d106dbe
Guilherme Canalli da Silva <guilherme.canalli5@gmail.com>
2018-08-10 06:57:44 +0200

    Created layout files for step and ingredient items of widget and added some configuration to the cookingstep widget service class.

--- app/src/main/java/com/example/android/bakingbuddy/widget/CookingStepsWidgetService.java
+++ app/src/main/java/com/example/android/bakingbuddy/widget/CookingStepsWidgetService.java
@@ -1,20 +1,39 @@
 package com.example.android.bakingbuddy.widget;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
+import android.widget.ListView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
+
+import com.example.android.bakingbuddy.R;
+import com.example.android.bakingbuddy.model.CookingStep;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CookingStepsWidgetService extends RemoteViewsService{
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return null;
+        return new CookingStepsRemoteViewsFactory(this.getApplicationContext(), intent);
     }
 
     class CookingStepsRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory{
+        private Context mContext;
+        private List<CookingStep> mCookingSteps = new ArrayList<>();
+        private int mAppWidgetId;
+
+        public CookingStepsRemoteViewsFactory(Context context, Intent intent){
+            mContext = context;
+            mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
 
         @Override
         public void onCreate() {
-
+//            TODO: Implement a way of getting the data from source
+//            TODO: Implement content provider? Suggestion: schematic
         }
 
         @Override
@@ -29,12 +48,14 @@ public class CookingStepsWidgetService extends RemoteViewsService{
 
         @Override
         public int getCount() {
-            return 0;
+            return mCookingSteps.size();
         }
 
         @Override
         public RemoteViews getViewAt(int position) {
-            return null;
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_cooking_step_item);
+            rv.setTextViewText(R.id.tv_widget_step_description, mCookingSteps.get(position).getDescription());
+            return rv;
         }
 
         @Override
